07efb5edfb3e4e694b1807bb3ee1f2170441670d
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-16 22:18:00 +0200

    creazione nuovo prodotto

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 include_once __DIR__ . '/entities/users.php';
+include_once __DIR__ . '/entities/product.php';
 
 $utente = new Utente('Johnny','Johnny@gmail.com');
 
 var_dump($utente);
+
+$antipulci = new Prodotto('Antipulci', '2 pezzi', '10');
+
+var_dump($antipulci);
 
 ?>
